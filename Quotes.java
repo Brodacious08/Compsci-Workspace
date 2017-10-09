e309721fa7cc5926b8102cc891d4560394645f78
Brodacious08 <32646798+Brodacious08@users.noreply.github.com>
2017-10-09 10:42:44 -0600

    Add files via upload
    this was a test upload to git

---
+++ Quotes.java
@@ -0,0 +1,18 @@
+/*
+ * Thisclass is used to prin quotes
+ */
+public class Quotes {
+	
+	/*
+	 * every class will need a constructor
+	 */
+	public Quotes() {
+		System.out.println("Debug: quotes constructor called");
+	
+	}
+	
+	void sayit() {
+		System.out.println("Honesty is the best policy.");
+	}
+
+}
